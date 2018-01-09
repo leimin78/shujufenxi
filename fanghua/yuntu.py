ed28e1321b74cefcc5d7dfb8b3931644f4e3d895
leimin78 <18898761417@139.com>
2018-01-09 15:08:53 +0800

    更改readme.md

--- fanghua/yuntu.py
+++ fanghua/yuntu.py
@@ -13,23 +13,26 @@ from dbHandle import dbHandle
 
 def getContent(table_name):
     db = dbHandle()
+    jieba.del_word("电影")
+    jieba.del_word("导演")
+    jieba.del_word("没有")
+    jieba.del_word("影片")
+    jieba.del_word("看到")
     query_sql = "select comment_content from {0}".format(table_name)
     texts = db.query_db(query_sql)
     text = ''
     for t in texts:
         text += t[0]
-    result = analyse.textrank(text, topK=1000, withWeight=True)
-    keywords = dict()
-    for i in result:
-        keywords[i[0]] = i[1]
-    print(keywords)
-    return keywords
+    #result = jieba.analyse.textrank(text, topK=1000, withWeight=True)
+    result = jieba.cut(text,cut_all=True)
+    wl_space_split = " ".join(result)
+    return wl_space_split
 
 def getwordCloud(abs_filename,keywords):
     image = Image.open(abs_filename)
     graph = np.array(image)
     wc = WordCloud(font_path='/Users/leimin/spiderCrawl/shuju/shujufenxi/fanghua/simhei.ttf',background_color='white', max_words=1000, mask=graph)
-    wc.generate_from_frequencies(keywords)
+    wc.generate(keywords)
     image_color = ImageColorGenerator(graph)
 
     # 显示图片
--- readme.md
+++ readme.md
@@ -1,19 +1,21 @@
 ### 本项目主要记录自己学习数据采集,数据维度分析,及数据可视化
 
-##子项目1  拉勾网,深圳 职位分析
+### 子项目1 拉勾网,深圳 职位分析
 
-
+----
 allCategory.py 获取所有岗位分类，对分类名及url链接进行入库
 
 #职位分类表结构,分类名,分类URL
-
+```
 CREATE TABLE jobCategories
 (
 category_name varchar(100),
 category_url varchar(500)
 )
+```
 
 #公司属性表,公司ID,公司所在城市,所在区域,融资情况,行业,公司大小,公司标签,公司名
+```
 create table company_detail
 (
     companyId int(100) not null unique,
@@ -25,9 +27,9 @@ create table company_detail
     companyLabelList varchar(200),
     companyFullName varchar(100)
 )
-
+```
 #职位属性表,职位id,职位名,工作年限,教育情况,职位所属公司id,薪资,公司福利,创建时间,职位标签,职位描述,职位所属分类
-
+```
 create table position_detail
 (
     positionId int(100) not null unique,
@@ -42,5 +44,6 @@ create table position_detail
     positionText varchar(1000),
     caregory
 )
+```
 
 项目分析文章--http://www.jianshu.com/p/83dea1b4aa27
